e002a68fe1a82ab1a77906a0a0a32dc450c7b21a
tahminabagirova <tehmine45bagirova@gmail.com>
2023-02-20 18:46:37 +0400

    brown btn created && main section try for free button added

--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -11,7 +11,8 @@ const Homepage = () => {
             <div className="col-md-5 content-side">
               <h1>Bütün işlərinizi bir hesabda birləşdirdik! </h1>
               <p>Müxtəlif sektorda olan sahibkarlara biznes proseslərinin idarəsini asanlaşdıracaq rəqəmsal biznes həlləri</p>
-              <button className="btn btn-outline">Elə indi təklif al!</button>
+              <button className="btn btn-outline mr-3">Elə indi təklif al!</button>
+              <button className="btn btn-brown">Pulsuz test et</button>
             </div>
             <div className="col-md-7 image-side"></div>
           </div>
