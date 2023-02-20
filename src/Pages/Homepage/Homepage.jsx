dff07162e3ccb6a1dff045985074715244f534df
tahminabagirova <tehmine45bagirova@gmail.com>
2023-02-20 16:44:23 +0400

    main section contents added && variables css created and imported

--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -7,7 +7,14 @@ const Homepage = () => {
   return (
     <div className="container">
       <div className="main-section">
-
+          <div className="row align-items-center">
+            <div className="col-md-5 content-side">
+              <h1>Bütün işlərinizi bir hesabda birləşdirdik! </h1>
+              <p>Müxtəlif sektorda olan sahibkarlara biznes proseslərinin idarəsini asanlaşdıracaq rəqəmsal biznes həlləri</p>
+              <button class="btn btn-outline">Elə indi təklif al!</button>
+            </div>
+            <div className="col-md-7 image-side"></div>
+          </div>
       </div>
     </div>
   );
