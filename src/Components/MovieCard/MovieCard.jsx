13b7e887f2285446fac9ede215e5a1e8152a2a45
nethreen <nezrinsoltanli@gmail.com>
2023-12-06 20:49:19 +0400

    map

--- src/Components/MovieCard/MovieCard.jsx
+++ src/Components/MovieCard/MovieCard.jsx
@@ -1,23 +1,23 @@
 import React, { useEffect } from "react";
 import "./MovieCard.scss";
 
+const MovieCard = ({ movie }) => {
+  const getMovies = () => {
+    fetch(
+      "https://api.themoviedb.org/3/discover/movie?api_key=407de4dfd89db769eddb39c9ab90c177"
+    )
+      .then((res) => res.json())
+      .then((json) => console.log(json));
+  };
 
-const MovieCard = () => {
+  useEffect(() => {
+    getMovies();
+  }, []);
 
-    const getMovies = () => {
-        fetch("https://api.themoviedb.org/3/discover/movie?api_key=407de4dfd89db769eddb39c9ab90c177")
-        .then(res => res.json())
-        .then(json => console.log(json))
-    }
-
-    useEffect(() => {
-        getMovies()
-    },[])
-  
   return (
     <div className="movie-card">
-        <img src="" alt="" />
-        test
+      <img src="" alt="" />
+      test
     </div>
   );
 };
--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -8,43 +8,54 @@ import MovieCard from "../../Components/MovieCard/MovieCard";
 // import { Pagination, Navigation } from "react-swipe";
 
 const Homepage = () => {
-
   const [showBtnTop, setScroll] = useState(false);
+  const [movies, setMovies] = useState([]);
   useEffect(() => {
-   window.addEventListener("scroll", () => {
-     setScroll(window.scrollY > 50);
-   });
+    window.addEventListener("scroll", () => {
+      setScroll(window.scrollY > 50);
+    });
   }, []);
 
   const scrollTop = () => {
     window.scrollTo({
-        top: 0,
-        behavior: "smooth",
+      top: 0,
+      behavior: "smooth",
     });
   };
 
   return (
     <div className="home-page-main">
-      <button className={`btn-up ${showBtnTop ? "show" : ""}`} onClick={scrollTop}><DblChevron/></button>
-      
+      <button
+        className={`btn-up ${showBtnTop ? "show" : ""}`}
+        onClick={scrollTop}
+      >
+        <DblChevron />
+      </button>
+
       <div className="container">
         <section className="main-section">
-            <div className="row align-items-center">
-              <div className="col-md-5 content-side">
-                <h1>Lorem ipsum dolor, sit amet consectetur adipisicing.</h1>
-                <p>Sunt est nihil consequatur, dolore, facere possimus blanditiis.</p>
-                <button className="btn btn-outline mr-3">Apply</button>
-                <button className="btn btn-brown">Free test</button>
-              </div>
-              <div className="col-md-7 image-side"></div>
+          <div className="row align-items-center">
+            <div className="col-md-5 content-side">
+              <h1>Lorem ipsum dolor, sit amet consectetur adipisicing.</h1>
+              <p>
+                Sunt est nihil consequatur, dolore, facere possimus blanditiis.
+              </p>
+              <button className="btn btn-outline mr-3">Apply</button>
+              <button className="btn btn-brown">Free test</button>
             </div>
+            <div className="col-md-7 image-side"></div>
+          </div>
         </section>
 
         <section>
           <div className="row">
-            <div className="col-md-4">
-              <MovieCard></MovieCard>
-            </div>
+            {movies.map((movie) => {
+              return (
+                <div className="col-md-4" key={movie}>
+                  <MovieCard movie={movie} />
+                </div>
+              );
+            })}
           </div>
         </section>
       </div>
