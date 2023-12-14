01892b7cbce013254a64e96f59e69749c122c258
tahminabagirova <tehmine45bagirova@gmail.com>
2023-12-15 02:08:25 +0400

    add to favourites not completed

--- src/Components/MovieCard/MovieCard.jsx
+++ src/Components/MovieCard/MovieCard.jsx
@@ -1,16 +1,23 @@
 import React from "react";
+import ApiService from "../../api/axios";
 import "./MovieCard.scss";
 import "@flaticon/flaticon-uicons/css/all/all.css";
 import 'react-tooltip/dist/react-tooltip.css';
 
-
 const MovieCard = ({ movie }) => {
+  const addToFavourites = (e) => {
+    e.preventDefault();
+    ApiService.post(`3/account/20694159/favorite?api_key=407de4dfd89db769eddb39c9ab90c177`).then((resp) => {
+      window.location.reload();
+      console.log(movie.id);
+    });
+  };
   return (
     <div className="movie-card">
       <img src={"https://image.tmdb.org/t/p/w500" + movie.poster_path} alt="" />
       <div className="overlay">
         <div className="tools">
-          <button data-tooltip-id="my-tooltip" data-tooltip-content="Hello world!">
+          <button onClick={(e) => addToFavourites(e, movie.id)}>
             <i className="fi fi-br-plus"></i>
           </button>
         </div>
